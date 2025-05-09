19a5ac46c0e08f4b369ee30229070e8b39352323
its-mrarsikk <mr.arsikoff@gmail.com>
2025-05-09 13:37:43 +0700

    initial (template)

---
+++ src/client/java/mod/mcclock/MCClockClient.java
@@ -0,0 +1,10 @@
+package mod.mcclock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCClockClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
